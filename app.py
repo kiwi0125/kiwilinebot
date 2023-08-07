f88648327e6f4e3e6b6feb2417a92c5a658053eb
kiwi0125 <139112648+kiwi0125@users.noreply.github.com>
2023-08-07 11:52:49 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -28,11 +28,49 @@ def callback():
 
     return "OK"
 
-#處理訊息
+#處理訊息(這邊是回傳同樣的訊息)
+# @handler.add(MessageEvent, message=TextMessage)
+# def handle_message(event):
+#     message = TextSendMessage(text=event.message.text)
+#     line_bot_api.reply_message(event.reply_token, message)
+
+#處理訊息(這邊是歡迎訊息)
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
+    emoji = [
+            {"index":0,
+             "productId":"5ac21b4f031a6752fb806d59",
+             "emojiId":"011"
+                },
+
+            {"index":1,
+             "productId":"5ac21b4f031a6752fb806d59",
+             "emojiId":"035"
+                },
+            {"index":2,
+             "productId":"5ac21b4f031a6752fb806d59",
+             "emojiId":"049"
+                },
+            {"index":3,
+             "productId":"5ac21b4f031a6752fb806d59",
+             "emojiId":"035"
+                },
+            ]
+    #歡迎訊息
+    text_message = TextSendMessage(text="""$$$$
+Hello! 恭喜您成為kiwi機器人的第100個好友
+這邊提供了一些功能，這段只是歡迎訊息而已
+才不希望你點進來呢！""",emojis=emoji)
+    
+    #貼圖訊息
+    sticker_message = StickerMessage(
+        package_id = "11537",
+        sticker_id = "52002769")
+    
+    #將歡迎訊息、貼圖訊息放到機器人裡面回復
+    line_bot_api.reply_message(
+        event.reply_token,
+        [text_message, sticker_message])
 
 if __name__=="__main__":
     app.run()
