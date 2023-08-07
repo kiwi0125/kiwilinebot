e9ab7e27defd64795d53ab79e29420dbd3b95bbb
kiwi0125 <139112648+kiwi0125@users.noreply.github.com>
2023-08-07 10:24:14 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, abort
+from linebot import (LineBotApi, WebhookHandler, exceptions)
+from linebot.exceptions import (InvalidSignatureError)
+from linebot.models import *
+
+app = Flask(__name__)
+
+#自己的Channel Access Token(在Messaging API底下)
+line_bot_api = LineBotApi("wR6RWNy+d1LEYGfPCD0AbGehrEI+cPTQxChN5KftrpfD7JZbuwKIoj1Ys41AL8+S2tehpIAOJVeZihxBVyZnMi8YPeHpQT9PeMRzc+UfkGwoxcSYIc9H+5yLPh3HSvsR4cagMIIFHybDESjA0+CiewdB04t89/1O/w1cDnyilFU=")
+#自己的Channel secret(在Basic Settings底下)
+handler = WebhookHandler("5c7720c80fc66096c32f76253778ded8")
+
+#監聽所有來自/callback的Post Request
+@app.route("/callback", method=["POST"])
+def callback():
+    #get X-Line-Signature header value
+    signature = request.headers["X-Line_Signature"]
+
+    #get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    #header webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return "OK"
+
+#處理訊息
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token, message)
+
+if __name__=="__main__":
+    app.run()
